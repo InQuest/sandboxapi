4cd46fb7bd2813a2edbdea58bdf65566f0872b42
rshipp <github@rshipp.com>
2018-12-10 11:59:59 -0600

    Bump version to v1.4.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = requirements.strip().split('\n')
 
 setup(
     name='sandboxapi',
-    version='1.4.2',
+    version='1.4.3',
     include_package_data=True,
     packages=[
         'sandboxapi',
